2a27c0a1383961dfbd5fea05b30e4b4968ddd27a
Coke1063 <141168240+Coke1063@users.noreply.github.com>
2024-05-07 22:21:34 -0500

    optimized normal mode. also made msg boxes more convenient

--- Message_Spammer.py
+++ Message_Spammer.py
@@ -3,10 +3,12 @@ import time
 import keyboard as key
 
 
+rmodetoggle = False
+
+
 def def_factors():
-    if pt.confirm('Would you like to enable rant mode?') == 'OK':
-        rantmode()
-        return
+    if pt.confirm('Select mode:', buttons=['Rant', 'Normal']) == 'Rant':
+        return rantmode()
     else:
         pass
     global amount
@@ -30,6 +32,8 @@ def def_factors():
 
 
 def rantmode():
+    global rmodetoggle
+    rmodetoggle = True
     rant = pt.prompt('Enter your rant:')
     if rant is not None:
         pass
@@ -41,9 +45,17 @@ def rantmode():
 
 
 def confirmprompt():
-    prompt = pt.confirm('Please make sure that when you press enter the message is sent before continuing. YOU CAN '
-                        'STOP AT ANY TIME BY MOVING'
-                        'YOUR MOUSE TO ANY CORNER OF YOUR SCREEN.')
+    if paste_clip != 'OK':
+        prompt = pt.confirm(
+            'Please make sure that when you press enter the message is sent before continuing. NOTE: This '
+            'will move your mouse back and forth to optimize input. YOU CAN '
+            'STOP AT ANY TIME BY MOVING '
+            'YOUR MOUSE TO ANY CORNER OF YOUR SCREEN.')
+    else:
+        prompt = pt.confirm(
+            'Please make sure that when you press enter the message is sent before continuing. YOU CAN '
+            'STOP AT ANY TIME BY MOVING '
+            'YOUR MOUSE TO ANY CORNER OF YOUR SCREEN.')
     if prompt == 'OK':
         spam_init()
     else:
@@ -63,11 +75,24 @@ def confirmpromptrmode():
 def spam_init():
     i = 0
     if paste_clip != 'OK':
+        b = 0
         time.sleep(2)
         while i < int(amount):
-            pt.typewrite(message)
-            pt.press('enter')
-            i += 1
+            if b == 0:
+                pt.moveTo(806, 554, 0.3)
+                pt.typewrite(message)
+                pt.press('enter')
+                i += 1
+                b = 1
+                continue
+            else:
+                pt.moveTo(1139, 437, 0.3)
+                pt.typewrite(message)
+                pt.press('enter')
+                i += 1
+                b = 0
+                continue
+        pt.moveTo(900, 600, 0.3)
     else:
         time.sleep(2)
         while i < int(amount):
@@ -78,9 +103,9 @@ def spam_init():
 
 def rmode_init():
     time.sleep(2)
-    global a
     a = 0
     try:
+        global splitrant
         for words in splitrant:
             if a == 0:
                 a = 1
@@ -95,15 +120,19 @@ def rmode_init():
                 key.press('enter')
                 continue
     except:
-        pt.alert("An error has ocurred. (Mostly as an attempt to debug on Azaoki's pc")
+        pt.alert("An error has ocurred. this also shows up when you move your mouse to the corner.(Mostly as an "
+                 "attempt to debug on Azaoki's pc)")
         exit()
     pt.moveTo(900, 600, 0.3)
 
 
 def_factors()
 while True:
-    prompt2 = pt.confirm('Rerun with current parameters? Only works in non-rant mode.')
+    prompt2 = pt.confirm('Rerun with current parameters?')
     if prompt2 == 'OK':
-        spam_init()
+        if rmodetoggle:
+            confirmpromptrmode()
+        else:
+            confirmprompt()
     else:
         exit()
